ef045ae889bf444bcdf23b6f3dacf94d07574872
POLOWB <wabbaye50paul@gmail.com>
2024-08-27 12:32:46 +0300

    implemented auth-callback

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -10,10 +10,22 @@ import { ArrowRight, Check } from 'lucide-react'
 import { BASE_PRICE, PRODUCT_PRICES } from '@/config/products'
 import { Button } from '@/components/ui/button'
 import { useMutation } from '@tanstack/react-query'
+import { createCheckoutSession } from './actions'
+import { useRouter } from 'next/navigation'
+import { useToast } from '@/components/ui/use-toast'
+import {useKindeBrowserClient} from'@kinde-oss/kinde-auth-nextjs'
+import LoginModal from '@/components/LoginModal'
 
 
 const DesignPreview = ({configuration}:{configuration:Configuration}) => {
-  const [showConfetti, setShowConfetti]=useState(false)
+  const router = useRouter()
+  const {toast} = useToast()
+  const {id} = configuration
+  const {user} = useKindeBrowserClient()
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState<boolean>(false)
+
+
+  const [showConfetti, setShowConfetti]=useState<boolean>(false)
 
   useEffect(()=> setShowConfetti(true))
 
@@ -28,15 +40,41 @@ const DesignPreview = ({configuration}:{configuration:Configuration}) => {
 
   if(finish === "textured") totalPrice += PRODUCT_PRICES.finish.textured
 
-  //const {} = useMutation({
-    //mutationKey: ["get-checkout-session"],
-    //mutationFn:
-  //})
+  const {mutate: createPaymentSession} = useMutation({
+    mutationKey: ["get-checkout-session"],
+    mutationFn: createCheckoutSession,
+    onSuccess: ({url})=>{
+      if(url) router.push(url)
+        else throw new Error("Unable to retrieve payment URL.")
+    },
+    onError: () => {
+      toast({
+        title: "Something went wrong",
+        description: "There was an error on our end. Please try again.",
+        variant: 'destructive',
+      })
+    }
+  })
+
+  const handleCheckout = () =>{
+    if(user){
+      //create payment session
+      createPaymentSession({configId: id})
+    }else{
+      //need to log in
+      
+      localStorage.setItem('configurationId',id)
+      setIsLoginModalOpen(true)
+    }
+  }
 
  return<>
   <div aria-hidden='true' className="pointer-events-none select-none absolute inset-0 overflow-hidden flex justify-center">
     <Confetti active={showConfetti} config={{elementCount:200, spread: 90}}/>
   </div>
+
+  <LoginModal isOpen={isLoginModalOpen} setIsOpen={setIsLoginModalOpen}/>
+  
   <div className='mt-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12'>
     <div className='sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2'>
       <Phone className={cn("bg-${tw}")}imgSrc={configuration.croppedImageUrl!}/>
@@ -105,7 +143,7 @@ const DesignPreview = ({configuration}:{configuration:Configuration}) => {
           </div>
         </div>
         <div className='mt-8 flex justify-end pb-12'>
-            <Button className='px-4 sm:px-6 lg:px-8'>
+            <Button onClick={()=>handleCheckout()} className='px-4 sm:px-6 lg:px-8'>
               Check out <ArrowRight className='H-4 W-4 ml-1.5 inline'/>
             </Button>
         </div>
